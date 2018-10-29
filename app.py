edeee8885f7d9a4c055b578e7a48c4b18cec9871
Dan <dlaubacher3@gmail.com>
2018-10-28 20:12:38 -0400

    add db health check

--- app.py
+++ app.py
@@ -16,15 +16,33 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 app.config['FLASK_DEBUG'] = 1
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:root@db:5432/flaskapp'
 db = SQLAlchemy(app)
 
 health = HealthCheck(app, "/healthcheck")
 envdump = EnvironmentDump(app, "/environment")
 
+def database_status():
+    working = True
+    output = 'database is ok'
+
+    try:
+        session = db.session()
+        session.execute('SELECT 1')
+    except Exception as e:
+        output = str(e)
+        working = False
+
+    return working, output
+
+health.add_check(database_status)
+
+
 @app.route("/ping", methods=['GET'])
 def hello():
     return "pong"
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+    
